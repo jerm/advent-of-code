0cbbc1b7c24201471a1890d80d97654c768f21b4
Jeremy Price <jeremy@grokability.com>
2022-12-06 23:57:43 -0800

    make market length more easily configurable

--- 2022/day6.py
+++ 2022/day6.py
@@ -2,14 +2,15 @@
 
 pos = 0
 my_slice = []
+marker_length = 14
 
 with open(f"day6.txt", "r") as fp:
     stream = fp.read()
 
 for pos in range(len(stream)):
     my_slice.append(stream[pos])
-    if len(my_slice) == 14:
-        if len(set(my_slice)) == 14:
+    if len(my_slice) == marker_length:
+        if len(set(my_slice)) == marker_length:
             print(my_slice)
             print(pos+1)
         else:
